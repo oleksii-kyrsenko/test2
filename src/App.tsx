a88d7cbdb74fbe3065114a345e1e8c550d97d00c
kirsenkodanit <a.kirs@ukr.net>
2020-06-11 11:08:05 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test2 deploy again
+          New
         </p>
         <a
           className="App-link"
