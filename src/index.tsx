96ea362c02d0c3d0c77e97dc9b6ede14fd74976a
kirsenkodanit <a.kirs@ukr.net>
2020-06-24 19:11:20 +0300

    store refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import './css/reset.css';
 import './css/index.css';
 import { App } from './App';
-import store from './store/index';
+import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { rootReducer } from './reducers';
-import { rootSaga } from './sagas';
+import { rootReducer } from './rootReducer';
+import { rootSaga } from './rootSaga';
 
 declare global {
   // tslint:disable-next-line: interface-name
